6825764878e5ca2b7051f4e130c4fdf6086ed5d9
Deepak <lahiripak@gmail.com>
2016-10-31 18:27:19 +0000

    Added SqliteAuthenticationMixin

--- cmd_line_tools/mixins/auth.py
+++ cmd_line_tools/mixins/auth.py
@@ -36,3 +36,33 @@ class SimpleAuthenticationMixin(object):
 # Can you think two more authentication services?
 # A Json based service and one based on a sqlite3 database?
 # Both are builtin modules in Python, should be easy ;)
+
+
+#Sqlite3 Authentication
+
+import sqlite3
+class SqliteAuthenticationMixin(object):
+    """Create database connection to sqlite and check for username password"""
+    
+    def create_connection(self):
+        self.con = sqlite3.connect("UserDataBase.db") #sqlite3 has no connect member??
+        self.c = self.connection.cursor()
+        return self.c
+        
+    def create_username_password_table(self):
+        self.c = self.create_connection()
+        self.c.execute('''CREATE TABLE Users (username text, password text)''')
+        self.connection.commit()
+        
+    def set_username_password(self, username, password):
+        self.c = self.create_connection()
+        self.c.execute("INSERT INTO Users VALUES(username, password)")
+        self.connection.commit()
+        
+        
+    def is_authenticated(self,username, password):
+        self.c = self.create_connection()
+        password_from_db = self.c.execute("SELECT password FROM Users WHERE username = {}".format(password))
+        if password == password_from_db:
+            return True
+        return False
