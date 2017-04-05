e01ecb165537978d7eccbe7c404df7d0b612f04e
Michael Netherland <michaeljnetherland@gmail.com>
2017-04-05 02:12:12 +0000

    Command Line initial

--- cmd_line_tools/mixins/__init__.py
+++ cmd_line_tools/mixins/__init__.py
@@ -1,4 +1,4 @@
 from .data_request_mixins import InputRequestMixin, ArgumentsRequestMixin
 from .parser_mixins import SimpleCommandLineParserMixin
-from .output import StdoutOutputMixin, FileOutputMixin
+from .output import StdoutOutputMixin, FileOutputMixin, OutputJSONMixin
 from .auth import LoginMixin, SimpleAuthenticationMixin
