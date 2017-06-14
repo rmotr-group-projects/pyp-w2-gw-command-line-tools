5f3011a522b3e4e272ffb1b0cdef9e5ca220f3b9
Marianna <mangat.marianna@gmail.com>
2017-06-14 03:04:06 +0000

    Tests passed in Python27

--- cmd_line_tools/mixins/parser_mixins.py
+++ cmd_line_tools/mixins/parser_mixins.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 __all__ = ['SimpleCommandLineParserMixin']
 
@@ -7,15 +8,56 @@ class SimpleCommandLineParserMixin(object):
     ARGUMENTS_ATTR_NAME = '_arguments'
 
     def parse_arguments(self):
+       
         arguments = getattr(self, self.ARGUMENTS_ATTR_NAME, {})
-
+        # why not just doing arguments = {} ?
+        # '''
+        # if hasattr(self, self.ARGUMENTS_ATTR_NAME):
+        #     arguments = self._arguments
+        # else:
+        #     arguments = {}
+        # '''
+        # 1) self._arguments = {}
         for argv in sys.argv:
             if '=' in argv:
                 arguments.update(dict([tuple(argv.split('='))]))
 
         # Equivalent:
         # self._arguments = arguments
-        setattr(self, self.ARGUMENTS_ATTR_NAME, arguments)
-
+        setattr(self, self.ARGUMENTS_ATTR_NAME, arguments) 
+        # 2) ===> self._arguments = arguments
 
 # Can you implement an argparse version?
+
+class AdvancedCommandLineParserMixin(object):
+    ARGUMENTS_ATTR_NAME = '_arguments'
+
+    def parse_arguments(self):   
+        arguments = getattr(self, self.ARGUMENTS_ATTR_NAME, {})
+        
+        # print(my_object.my_attribute)
+        # print(get_attr(my_object, 'my_attribute', 'Hello'))
+        
+        parser = argparse.ArgumentParser()
+        
+        for argv in sys.argv:
+            if '--' in argv:
+                parser.add_argument(argv)
+                
+        parsed_args = parser.parse_args()  
+        
+        arguments.update(vars(parsed_args))
+      
+        
+        setattr(self, self.ARGUMENTS_ATTR_NAME, arguments)
+      
+        
+          # for op in OPERATIONS:
+        #     parser.add_argument("--{}".format(op))
+            
+        # python cmd.py arg1 arg2
+        # python cmd.py --arg1=val1
+# """
+# python my_command.py username=bob paassword=12345
+# python my_command.py --username bob --password 12345
+# """
